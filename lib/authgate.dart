83b197ec85c123094acb391f7f565085b2cf3fef
ndmichael <ukejemicheal@gmail.com>
2025-07-29 13:06:28 +0100

    manage errors and which screen to display depending on the user status

---
+++ lib/authgate.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:newhms/features/auth/screens/login_screen.dart';
+import 'package:newhms/features/home/home_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  Future<String?> _getUserRole(String uid) async {
+    try {
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .get();
+
+      if (doc.exists && doc.data() != null) {
+        return doc.get('role') as String?;
+      }
+    } catch (e) {
+      debugPrint("Error fetching role: $e");
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(), // auto updates
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        if (!snapshot.hasData) {
+          return const LoginScreen(); // User not logged in
+        }
+
+        // Fetch role before returning HomeScreen
+        return FutureBuilder<String?>(
+          future: _getUserRole(snapshot.data!.uid),
+          builder: (context, roleSnapshot) {
+            if (roleSnapshot.connectionState == ConnectionState.waiting) {
+              return const Scaffold(
+                body: Center(child: CircularProgressIndicator()),
+              );
+            }
+
+            // return if error
+            if (roleSnapshot.hasError || roleSnapshot.data == null) {
+              return const Scaffold(
+                body: Center(child: Text("Error loading user role.")),
+              );
+            }
+
+            return HomeScreen(role: roleSnapshot.data!); // return HomeScreen with role passed
+          },
+        );
+      },
+    );
+  }
+}
